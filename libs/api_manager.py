0ebe1b58953015bd4c285f57f35ae770f3533a26
jahyeonbeak <jahyeonbeak@gmail.com>
2018-03-28 14:55:01 +0800

    Fix bug

--- libs/api_manager.py
+++ libs/api_manager.py
@@ -6,7 +6,7 @@ import sys,os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../BitcoinMonitor/scripts/api').replace('\\', '/'))
 from bithumb.easy_api import EasyAPI
 import json
-from . import config
+import config
 
 class ApiManager(object):
     '''
